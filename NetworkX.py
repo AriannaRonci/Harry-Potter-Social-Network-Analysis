a9db262ad607a707d906e7abcd8108c505c6b9d6
FedericaParlapiano <s1094589@studenti.univpm.it>
2023-12-21 18:49:01 +0100

    eigenvector centrality

--- NetworkX.py
+++ NetworkX.py
@@ -1,3 +1,4 @@
+import matplotlib
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -38,17 +39,47 @@ bc_df = pd.DataFrame(bet.items(), columns=["keys", "values"])
 plot_order = bc_df.groupby('keys')['values'].sum().sort_values(ascending=False).index.values
 
 
-plt.figure(figsize=(16,8))
+plt.figure(figsize=(18,8))
 sns.barplot(x=list(bet.keys()), y=list(bet.values()), order=plot_order)
 plt.show()
 
+#closeness centrality
 clos = nx.closeness_centrality(graph)
 
 clos_df = pd.DataFrame(clos.items(), columns=["keys", "values"])
 plot_order = clos_df.groupby('keys')['values'].sum().sort_values(ascending=False).index.values
 
+'''values = clos_df.sort_values(["values"], ascending=False)
+matplotlib.rc_file_defaults()
 print(plot_order)
-
-plt.figure(figsize=(16, 8))
+ax1 = sns.set_style(style=None, rc=None)
+fig, ax1 = plt.subplots(figsize=(12,6))
+sns.lineplot(data=values["values"], marker='o', sort=True, ax=ax1)
+ax2 = ax1.twinx()'''
+plt.figure(figsize=(18,6))
 sns.barplot(x=list(clos.keys()), y=list(clos.values()), order=plot_order)
 plt.show()
+
+#degree centrality
+degree = nx.degree_centrality(graph)
+
+deg_df = pd.DataFrame(degree.items(), columns=["keys", "values"])
+plot_order = deg_df.groupby('keys')['values'].sum().sort_values(ascending=False).index.values
+
+plt.figure(figsize=(18,6))
+sns.barplot(x=list(degree.keys()), y=list(degree.values()), order=plot_order)
+plt.show()
+
+#eigenvector centrality
+eigen = nx.eigenvector_centrality(graph)
+
+eigen_df = pd.DataFrame(eigen.items(), columns=["keys", "values"])
+plot_order = eigen_df.groupby('keys')['values'].sum().sort_values(ascending=False).index.values
+
+plt.figure(figsize=(18,6))
+sns.barplot(x=list(eigen.keys()), y=list(eigen.values()), order=plot_order)
+plt.show()
+
+
+
+
