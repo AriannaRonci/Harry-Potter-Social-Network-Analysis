6d3abf69c5bdcd5532e0150f94724ea4d19f7626
FedericaParlapiano <s1094589@studenti.univpm.it>
2023-12-22 16:42:55 +0100

    description

--- NetworkX.py
+++ NetworkX.py
@@ -5,6 +5,28 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
 
+def description(graph):
+    print('\x1B[1m' + "Numero di nodi: " + '\x1B[0m' + f"{len(graph.nodes)}")
+    print('\x1B[1m' + "Numero di archi: " + '\x1B[0m' + f"{len(graph.edges)}")
+    print('\x1B[1m' + "Diametro: " + '\x1B[0m' + f"{nx.diameter(graph)}")
+    print('\x1B[1m' + "Grado medio: " + '\x1B[0m' + f"{sum(dict(graph.degree()).values()) / graph.number_of_nodes()}")
+    print('\x1B[1m' + "Densità: " + '\x1B[0m' + f"{nx.density(graph)}")
+    print('\x1B[1m' + "Coefficiente di clustering medio: " + '\x1B[0m' + f"{nx.average_clustering(graph)}")
+    print(
+        '\x1B[1m' + "Dimensione della componente connessa più ampia:" + '\x1B[0m' + f"{len(max(nx.connected_components(graph), key=len))}")
+    print('\x1B[1m' + "Connessione: " + '\x1B[0m' + f"{nx.is_connected(graph)}")
+
+    p = list(nx.periphery(graph))
+    print('\x1B[1m' + "Periferia del grafo:" + '\x1B[0m')
+
+    for i in range(0, len(p), 3):
+        if len(p) > i + 2:
+            print(f"       {mapping[p[i]]}, {mapping[p[i + 1]]}, {mapping[p[i + 2]]},")
+        elif len(p) > i + 1:
+            print(f"       {mapping[p[i]]}, {mapping[p[i + 1]]}")
+        elif len(p) > i:
+            print(f"       {mapping[p[i]]}")
+
 def draw_network(pos, type, edges_weight):
     plt.figure(3, figsize=(25, 25))
 
@@ -106,6 +128,8 @@ for i in range(0, len(source_list)):
 graph = nx.Graph()
 graph.add_edges_from(edges_list)
 
+description(graph)
+
 '''draw_network(nx.spring_layout(graph), "spring", edges["type"])
 draw_network(nx.kamada_kawai_layout(graph), "kamada", edges["type"])
 draw_network(nx.random_layout(graph), "random", edges["type"])
@@ -123,9 +147,9 @@ plot_centrality_distribution("Closeness", graph)
 plot_centrality_distribution("Degree", graph)
 plot_centrality_distribution("Eigenvector", graph)'''
 
-draw(graph, nx.spring_layout(graph), 'Betweeness')
+'''draw(graph, nx.spring_layout(graph), 'Betweeness')
 draw(graph, nx.spring_layout(graph), 'Closeness')
 draw(graph, nx.spring_layout(graph), 'Degree')
-draw(graph, nx.spring_layout(graph), 'Eigenvector')
+draw(graph, nx.spring_layout(graph), 'Eigenvector')'''
 
 
