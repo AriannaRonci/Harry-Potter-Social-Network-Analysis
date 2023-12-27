96fe2e51bbfcbfcd6e9939fac29f3d79943728d2
AriannaRonci <s1093926@studenti.univpm.it>
2023-12-27 16:49:21 +0100

    clique

--- CliqueKcore.py
+++ CliqueKcore.py
@@ -2,6 +2,7 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def k_core(graph):
     plt.figure(figsize=(20, 20))
     k_core_graph = nx.k_core(graph, k=None, core_number=None)
@@ -17,6 +18,7 @@ def k_core(graph):
     plt.savefig("grafici/k-core_network", bbox_inches="tight")
     plt.show()
 
+
 def ego_network(graph):
     plt.figure(figsize=(20, 20))
     ego_graph_hp = nx.ego_graph(graph, 39, radius=1, center=True, undirected=True, distance=None)
@@ -31,17 +33,63 @@ def ego_network(graph):
         if node == 'Harry Potter':
             color_map.append('red')
         else:
-            color_map.append('lightblue')
+            color_map.append('white')
 
     # Draw ego as large and red
     options = {"node_size": 400, "node_color": color_map}
     nx.draw_networkx_nodes(ego_graph_hp, pos, **options)
-    nx.draw_networkx_edges(ego_graph_hp, pos, width=0.5, edge_color='#E8E8E8')
+    nx.draw_networkx_edges(ego_graph_hp, pos, width=0.5)
     nx.draw_networkx_labels(ego_graph_hp, nx.spring_layout(ego_graph_hp, seed=1), font_size=15,
                             font_family='sans-serif', font_weight='bold')
     plt.box(False)
     plt.savefig("grafici/ego_network", bbox_inches="tight")
     plt.show()
+
+
+def clique(graph):
+    cliques = nx.find_cliques(graph)
+    print(list(cliques))
+    maximal_cliques_len = max(len(c) for c in nx.find_cliques(graph))
+    print({n: sum(1 for c in nx.find_cliques(graph) if n in c) for n in graph})
+
+    all_cliques = nx.enumerate_all_cliques(graph)
+    print("Cliques:" + str(len(list(all_cliques))))
+
+    max_clique = []
+    max_cliques = nx.find_cliques(graph)
+    number_max_cliques = sum(1 for c in max_cliques)
+    print("Maximal cliques: " + str(number_max_cliques))
+
+    clique11 = []
+    for c in nx.find_cliques(graph):
+        if len(c) == 11:
+            clique11.append(c)
+
+    i = 0
+    for clique in clique11:
+        plt.figure(figsize=(12, 8))
+        nodes = set(n for n in clique)
+        h = graph.subgraph(nodes)
+        deg = nx.degree(h)
+        nodes = [n for n in nodes if deg[n] == 10]
+        k = h.subgraph(nodes)
+        k = nx.relabel_nodes(k, mapping)
+        nx.draw(k, node_color="w", node_size=400, with_labels=True, font_weight='bold', font_size=15)
+        i = i + 1
+        plt.box(False)
+        plt.savefig(f"grafici/clique11_{i}", bbox_inches="tight")
+        # plt.show()
+
+    '''
+    print(max_clique)
+    clique_df = pd.DataFrame(max_clique,
+                             columns=["node1", "node2", "node3", "node4", "node5", "node6", "node7", "node8", "node9",
+                                      "node10", "node11"])
+    clique_df = clique_df.replace(mapping, regex=True)
+    print("Le clique massimali sono:")
+    for i in range(0, len(clique_df)):
+        print(clique_df.loc[i, :])'''
+
 
 characters = pd.read_csv("data/characters.csv")
 mapping = dict(zip(characters.id, characters.name))
@@ -61,32 +109,7 @@ for i in range(0, len(source_list)):
 graph = nx.Graph()
 graph.add_edges_from(edges_list)
 
-cliques = nx.find_cliques(graph)
-print(list(cliques))
-maximal_cliques_len = max(len(c) for c in nx.find_cliques(graph))
-print({n: sum(1 for c in nx.find_cliques(graph) if n in c) for n in graph})
-
-all_cliques = nx.enumerate_all_cliques(graph)
-print("Cliques:" + str(len(list(all_cliques))))
-
-max_clique = []
-max_cliques = nx.find_cliques(graph)
-number_max_cliques = sum(1 for c in max_cliques)
-print("Maximal cliques: " + str(number_max_cliques))
-for c in nx.find_cliques(graph):
-    if len(c) == 11:
-        print(c)
-        max_clique.append(c)
-
-print(max_clique)
-clique_df = pd.DataFrame(max_clique,
-                         columns=["node1", "node2", "node3", "node4", "node5", "node6", "node7", "node8", "node9",
-                                  "node10", "node11"])
-clique_df = clique_df.replace(mapping, regex=True)
-print("Le clique massimali sono:")
-for i in range(0, len(clique_df)):
-    print(clique_df.loc[i, :])
-
 
 ego_network(graph)
 k_core(graph)
+clique(graph)
